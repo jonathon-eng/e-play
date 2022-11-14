26e3dcca226c7ca9b2ab8604d977bc62cfebd943
Jonathon Silva Eng <54660634+jonathon-eng@users.noreply.github.com>
2022-11-13 20:32:55 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const nav = document.querySelector(".primary-navigation");
+const mobileButton = document.getElementById("mobileButton");
+
+mobileButton.addEventListener("click", () => {
+  const visibility = nav.getAttribute("data-something");
+  // if the nav is closed, open it. Use data-attributes for JS
+  if (visibility === "false") {
+    nav.setAttribute("data-something", "true");
+    mobileButton.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-something", "false");
+    mobileButton.setAttribute("aria-expanded", false);
+  }
+
+  console.log(visibility);
+
+  // if the nav is open, close it
+});
+
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
@@ -64,25 +83,6 @@ function hideContent(parent, content) {
     .querySelectorAll(content)
     .forEach((item) => panel.setAttribute("hidden", true));
 }
-
-const nav = document.querySelector(".primary-navigation");
-const mobileButton = document.getElementById("mobileButton");
-
-mobileButton.addEventListener("click", () => {
-  const visibility = nav.getAttribute("data-something");
-  // if the nav is closed, open it. Use data-attributes for JS
-  if (visibility === "false") {
-    nav.setAttribute("data-something", "true");
-    mobileButton.setAttribute("aria-expanded", true);
-  } else {
-    nav.setAttribute("data-something", "false");
-    mobileButton.setAttribute("aria-expanded", false);
-  }
-
-  console.log(visibility);
-
-  // if the nav is open, close it
-});
 
 // const jp_toggle = document.getElementById("jp");
 // const eng_toggle = document.getElementById("eng");
